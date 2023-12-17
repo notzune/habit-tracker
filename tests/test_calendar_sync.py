7f9a43799d9892f87a26c97992b2ecff5275ec43
zeyad rashed <brwnboy2002@gmail.com>
2023-12-17 11:05:45 -0500

    fix: modify existing unittests
    also creates one for time_tracking

--- tests/test_calendar_sync.py
+++ tests/test_calendar_sync.py
@@ -2,8 +2,7 @@ from unittest.mock import patch, Mock
 import unittest
 from habittracker.calendar_sync import create_or_update_event
 
-# Sample data for the test
-your_test_event_data = {
+test_event_data = {
     'start_time': '2021-01-01T09:00:00Z',
     'end_time': '2021-01-01T10:00:00Z',
     'summary': 'Test Event',
@@ -16,19 +15,29 @@ class TestCalendarSync(unittest.TestCase):
     @patch('habittracker.calendar_sync.get_calendar_service')
     def test_create_or_update_event(self, mock_service):
         # Mock the Google Calendar API service object
-        mock_service.return_value.events.return_value.insert.return_value.execute.return_value = {
+        mock_event = Mock()
+        mock_event.execute.return_value = {
             'id': '12345',
             'htmlLink': 'https://calendar.google.com/event?id=12345'
         }
+        mock_service.return_value.events.return_value.insert.return_value = mock_event
 
         # Call the function using the mock
         service = mock_service.return_value
-        result = create_or_update_event(service, your_test_event_data)
+        result = create_or_update_event(service, test_event_data)
 
-        # Assert that the service's insert method was called with the correct parameters
-        service.events.return_value.insert.assert_called_once()
-        # Assert that the result is not None
-        self.assertIsNotNone(result)
+        # Assert that the service's insert method was called with the expected parameters
+        service.events.return_value.insert.assert_called_once_with(
+            calendarId='primary',
+            body=test_event_data
+        )
+
+        # Assert that the result is correct
+        expected_result = {
+            'id': '12345',
+            'htmlLink': 'https://calendar.google.com/event?id=12345'
+        }
+        self.assertEqual(result, expected_result)
 
 
 if __name__ == '__main__':
--- tests/test_time_tracking.py
+++ tests/test_time_tracking.py
@@ -1,2 +1,40 @@
 import unittest
-from habittracker.time_tracking import get_active_window_title
+from unittest.mock import patch
+from habittracker.time_tracking import start_timer, stop_timer, get_time_spent, time_spent, last_app_name, last_start_time
+
+
+class TestTimeTracking(unittest.TestCase):
+
+    def setUp(self):
+        # Reset global variables before each test
+        time_spent.clear()
+        global last_app_name, last_start_time
+        last_app_name = None
+        last_start_time = None
+
+    @patch('your_module.time.time', side_effect=[100, 105])
+    def test_time_tracking(self, mock_time):
+        # Simulate starting and stopping the timer
+        start_timer("ExampleApp")
+        stop_timer()
+
+        # Check if time spent is recorded correctly
+        self.assertEqual(time_spent, {"ExampleApp": 5})
+
+        # Check if the last app name and last start time are reset
+        self.assertIsNone(last_app_name)
+        self.assertIsNone(last_start_time)
+
+    @patch('your_module.time.time', side_effect=[100, 110, 115])
+    def test_multiple_apps(self, mock_time):
+        # Simulate switching between apps
+        start_timer("ExampleApp")
+        start_timer("AnotherApp")
+        stop_timer()
+
+        # Check if time spent is recorded correctly
+        self.assertEqual(time_spent, {"ExampleApp": 10, "AnotherApp": 5})
+
+
+if __name__ == '__main__':
+    unittest.main()
